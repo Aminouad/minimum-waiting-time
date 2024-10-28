9e225bf16ce3c57dfca834f90fa660b0960812c4
Aminouad <ouadrani12@gmailcom>
2024-10-28 21:45:14 +0100

    Add project files.

---
+++ minimum-waiting-time/Program.cs
@@ -0,0 +1,35 @@
+﻿//O(nlog(n))T | O(1) space
+public class Program
+{
+    public int MinimumWaitingTime(int[] queries)
+    {
+        // Write your code here.
+        Array.Sort(queries);
+        int l = queries.Length;
+        int s = 0;
+        for (int i = 0; i < (l - 1); i++)
+        {
+            s = queries[i] * (l - i - 1) + s;
+        }
+        return s;
+    }
+}
+
+public class Program2
+{
+    public int MinimumWaitingTime(int[] queries)
+    {
+        // Write your code here.
+        Array.Sort(queries);
+        int l = queries.Length;
+        int s = 0;
+        int aux = 0;
+        for (int i = 0; i < (l - 1); i++)
+        {
+            aux = queries[i] + aux;
+            s = aux + s;
+        }
+        return s;
+    }
+}
+
